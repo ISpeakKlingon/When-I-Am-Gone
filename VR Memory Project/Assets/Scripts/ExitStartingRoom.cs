2a905ab509eb1ef78a991adefe5a1f98c5d6e263
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2021-12-01 18:35:26 -0500

    welcome lobby message
    Added a welcome audio cue when player enters lobby bridge.

--- VR Memory Project/Assets/Scripts/ExitStartingRoom.cs
+++ VR Memory Project/Assets/Scripts/ExitStartingRoom.cs
@@ -6,24 +6,23 @@ public class ExitStartingRoom : MonoBehaviour
 {
     public GameObject gameStartManagerObject;
     private GameStartManager gameStartManager;
+
     // Start is called before the first frame update
     void Start()
     {
         gameStartManager = gameStartManagerObject.GetComponent<GameStartManager>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (!GameManager.Instance.isExitedStartingRoom)
-        {
-            gameStartManager.ExitedStartingRoom(); //scene events get activated, like robot moving etc.
-            GameManager.Instance.ExitedStartingRoom(); //sets bool to true
-        }
+        if (other.CompareTag("Player"))
+            {
+                if (!GameManager.Instance.isExitedStartingRoom)
+                    {
+                        gameStartManager.ExitedStartingRoom(); //scene events get activated, like robot moving etc.
+                        GameManager.Instance.ExitedStartingRoom(); //sets bool to true
+                    }
+            }
+
     }
 }
