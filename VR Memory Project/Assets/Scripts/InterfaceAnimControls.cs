afc1b6fcdb79bdb0846409595ba66a7d113f284a
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2021-12-31 12:33:54 -0500

    light tip
    Added a red light to the tip of the Interface Docking cylinder.

--- VR Memory Project/Assets/Scripts/InterfaceAnimControls.cs
+++ VR Memory Project/Assets/Scripts/InterfaceAnimControls.cs
@@ -6,10 +6,21 @@ using UnityEngine.InputSystem;
 public class InterfaceAnimControls : HandAnimationControl
 {
     [SerializeField] Animator interfaceAnimator;
+    private float indicatorLightThreshhold = 0.99f;
+    public Renderer indicatorLight;
+    public Material indicatorOff, indicatorRed, indicatorGreen;
 
     public override void GripAnimation(InputAction.CallbackContext obj)
     {
         base.GripAnimation(obj);
         interfaceAnimator.SetFloat("Grip", obj.ReadValue<float>());
+        if(obj.ReadValue<float>() > indicatorLightThreshhold)
+        {
+            //turn indicator light on
+            if(indicatorLight.material != indicatorRed)
+            {
+                indicatorLight.material = indicatorRed;
+            }
+        }
     }
 }
