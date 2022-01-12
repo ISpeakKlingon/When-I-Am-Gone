84e99bc36d0fa915641586d24a05ac8ec33616b9
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2022-01-11 22:05:28 -0500

    Injector haptics
    HapticsController script added to Injectors. At the moment, it only sends a haptic impulse as the needle goes in/out.

---
+++ VR Memory Project/Assets/Scripts/InjectorHapticController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InjectorHapticController : MonoBehaviour
+{
+    public XRNode InjectorHandNode;
+    public XRBaseController InjectorController;
+
+    private void Start()
+    {
+        //InjectorController = GetComponentInParent<XRBaseController>();
+        InjectorController = GameManager.Instance.rightHandBaseController.GetComponent<XRBaseController>();
+    }
+
+    public void InjectorNeedleOn()
+    {
+        InjectorController.SendHapticImpulse(0.1f, 0.25f);
+        //Debug.Log("sent haptic impulse for InjectorNeedleOn method.");
+    }
+
+    public void InjectorNeedleOff()
+    {
+        InjectorController.SendHapticImpulse(0.1f, 0.25f);
+        //Debug.Log("sent haptic impulse for InjectorNeedleOff method.");
+    }
+
+}
