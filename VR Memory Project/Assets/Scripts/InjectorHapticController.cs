f8bd33e145f915b9fed2e2d9452396a8e4296628
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2022-01-14 22:36:07 -0500

    Injector haptics
    Added short haptics to Injector Docking/Undocking.

--- VR Memory Project/Assets/Scripts/InjectorHapticController.cs
+++ VR Memory Project/Assets/Scripts/InjectorHapticController.cs
@@ -8,11 +8,13 @@ public class InjectorHapticController : MonoBehaviour
 {
     public XRNode InjectorHandNode;
     public XRBaseController InjectorController;
+    public XRBaseController InterfaceController;
 
     private void Start()
     {
         //InjectorController = GetComponentInParent<XRBaseController>();
         InjectorController = GameManager.Instance.rightHandBaseController.GetComponent<XRBaseController>();
+        InterfaceController = GameManager.Instance.leftHandBaseController.GetComponent<XRBaseController>();
     }
 
     public void InjectorNeedleOn()
@@ -25,6 +27,16 @@ public class InjectorHapticController : MonoBehaviour
     {
         InjectorController.SendHapticImpulse(0.1f, 0.25f);
         //Debug.Log("sent haptic impulse for InjectorNeedleOff method.");
+    }
+
+    public void InjectorUndocking()
+    {
+        InterfaceController.SendHapticImpulse(.8f, .5f);
+    }
+
+    public void InjectorDocking()
+    {
+        InterfaceController.SendHapticImpulse(.8f, .5f);
     }
 
 }
