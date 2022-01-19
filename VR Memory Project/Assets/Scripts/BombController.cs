0618e72a50b5e38190595a60ad4d85b0b5fdf001
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2022-01-18 22:08:39 -0500

    Atomic flash
    Flash added to atomic bomb detonation.

--- VR Memory Project/Assets/Scripts/BombController.cs
+++ VR Memory Project/Assets/Scripts/BombController.cs
@@ -6,7 +6,10 @@ public class BombController : MonoBehaviour
 {
     public GameObject AtomicBomb;
     [SerializeField] private float _timeToDetonation = 10f;
-
+    [SerializeField] private AudioSource _audioSource;
+    public AudioClip Explosion;
+    [SerializeField] Animator _crossfadeAnim;
+    
     private void Awake()
     {
         AtomicBomb.SetActive(false);
@@ -14,12 +17,19 @@ public class BombController : MonoBehaviour
 
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         StartCoroutine(Detonate(_timeToDetonation));
+        _crossfadeAnim = GameManager.Instance.CrossfadeAnimator;
     }
 
     IEnumerator Detonate(float time)
     {
         yield return new WaitForSeconds(time);
+        _crossfadeAnim.SetTrigger("Flash");
+        yield return new WaitForSeconds(0.5f);
         AtomicBomb.SetActive(true);
+        _audioSource.PlayOneShot(Explosion);
     }
+
+
 }
--- VR Memory Project/Assets/Scripts/GameManager.cs
+++ VR Memory Project/Assets/Scripts/GameManager.cs
@@ -48,6 +48,8 @@ public class GameManager : MonoBehaviour
 
     public SkyboxController SkyboxController;
 
+    public Animator CrossfadeAnimator;
+
     public static GameManager Instance
     {
         get
